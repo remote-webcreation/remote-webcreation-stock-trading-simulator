fe47d107eca3770b67ecb38212d308d1cf8c5518
Mandy Jendroska <soulfreelancer@yahoo.com>
2025-05-24 18:35:26 +0200

    finihsed mainMenu & api.js

---
+++ src/api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://finnhub.io/api/v1';
+const API_KEY = process.env.FINNHUB_API_KEY;
+
+export async function getStockPriceData(symbol) {
+    try {
+        if (!API_KEY) {
+            throw new Error('FINNHUB_API_KEY is not defined in your .env file.');
+        }
+        const url = `${BASE_URL}/quote?symbol=${symbol}&token=${API_KEY}`;
+        const response = await axios.get(url);
+
+        if (
+            response.data && 
+            typeof response.data.c === 'number' &&
+            typeof response.data.pc === 'number'
+        ) {
+            return response.data;
+        } else {
+            throw new Error('No price found for this symbol.');
+        }
+    } catch (error) {
+        console.log('Error fetching stock price:', error.message);
+        return null;
+    }
+    
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,137 @@
 import inquirer from 'inquirer';
+import dotenv from 'dotenv';
+dotenv.config();
+import { fetchData } from './api.js';
+fetchData();
 import chalk from 'chalk';
 import { getStockPriceData } from './api.js';
 import { categories } from './symbols.js';
 import { buyAsset, showPortfolio } from './portfolio.js';
 
+async function mainMenu() {
+  try {
+    const { action } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'Choose from the menu below:\n',
+        choices: [
+          'Show stock price',
+          'Buy stock',
+          'Sell stock',
+          'Portfolio',
+          'History',
+          'Exit'
+        ]
+      }
+    ]);
+
+    switch (action) {
+      case 'Show stock price':
+
+        const { categoryName } = await inquirer.prompt([
+          {
+            type: 'list',
+            name: 'categoryName',
+            message: 'Choose a category:',
+            choices: categories.map(cat => cat.name)
+          }
+        ]);
+
+        // find category with name
+        const category = categories.find(cat => cat.name === categoryName);
+
+        if (!category.symbols || category.symbols.length === 0) {
+            console.log('\nNo symbols available in this category.\n');
+            break;
+        }
+
+        //choose symbol
+        const { symbol: choosenSymbol } = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'symbol',
+                message: 'Choose a symbol:',
+                choices: category.symbols.map(item => ({
+                    name: `${item.name} (${item.symbol})`,
+                    value: item.symbol
+                })).concat([
+                    new inquirer.Separator(),
+                    { name: 'Manuell eingeben', value: '__manual__' }
+                ])
+            }
+        ]);
+
+        // manual input
+        let finalSymbol = choosenSymbol;
+        if (choosenSymbol === '__manual__') {
+            const { manualSymbol } = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'manualSymbol',
+                    message: 'Enter stock/crypto symbol:'
+                }
+            ]);
+            finalSymbol = manualSymbol;
+        }
+
+        // request price
+        const priceData = await getStockPriceData(finalSymbol);
+
+        console.log('API response:', priceData);
+
+        if (priceData && typeof priceData.c === 'number' && typeof priceData.pc === 'number') {
+            const diff = priceData.c - priceData.pc;
+            let colorFunc = chalk.white;
+            if (diff > 0) colorFunc = chalk.green;
+            else if (diff < 0) colorFunc = chalk.red;
+            console.log(
+                `\nCurrent price of ${finalSymbol.toUpperCase()}: ${colorFunc(`$${priceData.c}`)}`
+            );
+        } else {
+            console.log('\nCould not fetch price. Try another symbol.\n');
+        }
+        break;
+      case 'Buy stock':
+
+      // choose category
+        const { buySymbol, amount } = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'buySymbol',
+                message: 'Enter the symbol you want to buy:'
+            },
+            {
+                type: 'number',
+                name: 'amount',
+                message: 'How many units do you want to buy?',
+                validate: input => input > 0 ? true : 'Enter a positive number.'
+            }
+        ]);
+        await buyAsset(buySymbol, amount);
+        break;
+      case 'Sell stock':
+        console.log("Feature coming soon!\n");
+        break;
+      case 'Portfolio':
+        await showPortfolio();
+        break;
+      case 'Show history':
+        console.log("Feature coming soon!\n");
+        break;
+      case 'Exit':
+        process.exit();
+    }
+    await mainMenu();
+
+  } catch (error) {
+    if (error.isTtyError || error.message?.startsWith('User force closed the prompt')) {
+      console.log('\nApplication closed.');
+      process.exit();
+    } else {
+      console.error('An error occurred:', error);
+      process.exit(1);
+    }
+  }
+}
+mainMenu();
